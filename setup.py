ea90a4ffc565cecf266c87163469409a49b89075
rodrigoprior <rprior@infranology.com.br>
2013-07-23 23:35:31 -0300

    update information and versions

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-ifgy-login',
-    version='0.1',
+    version='0.3',
     packages=['ifgy_login'],
     include_package_data=True,
     license='BSD License',
@@ -22,10 +22,13 @@ setup(
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Natural Language :: Portuguese (Brazilian)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
